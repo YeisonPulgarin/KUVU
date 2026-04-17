86a52bc8dd68878c9c65aac4d811e3188136f330
yeison <yeisonpulgarin07@gmail.com>
2026-04-17 11:40:03 -0500

    cambios

--- fronted/proyecto_angular/src/app/services/sidebar.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable, signal } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class SidebarService {
-  colapsado = signal(false);
-  toggle() { this.colapsado.update(v => !v); }
-}
